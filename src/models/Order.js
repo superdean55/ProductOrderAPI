e32d0c12af0548c1d66e27a2f33bd914405b212b
superdean55 <superdean55@gmail.com>
2025-10-26 17:19:47 +0100

    feat(models): add Order model with status, totalPrice and userId

---
+++ src/models/Order.js
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database.js";
+
+export default (sequelize) => {
+  class Order extends Model {
+    static associate(models) {
+        Order.belongsTo(models.User,{ foreignKey: 'userId'});
+    }
+  }
+  Order.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("PENDING", "PAID", "SHIPPED", "CANCELLED"),
+        defaultValue: "PENDING",
+      },
+      totalPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Order",
+      tableName: "orders",
+      timestamps: true,
+      underscored: true,
+    }
+  );
+  return Order;
+};
